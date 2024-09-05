ac72d1b157004f40e8ca202089b8b7aa97a1e45d
Steban Delgado <sdelgadobarrios@gmail.com>
2024-09-04 22:37:05 -0500

    Mejoras en la apariencia de inicio

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  navbarToggler.classList.add('collapsed');
+});
+
 document.querySelector('.navbar-toggler').addEventListener('click', function() {
-    this.classList.toggle('collapsed');
-  });
-  
+  this.classList.toggle('collapsed');
+});
